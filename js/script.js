914fa3eed3d13016bd268b2e790ff7d6427ba91b
safarboyalxorazmiy <safarboyalkhwarizmi@gmail.com>
2024-06-05 14:36:44 +0500

    Routing

--- js/script.js
+++ js/script.js
@@ -25,6 +25,8 @@ menuIcons[1].addEventListener("click", () => {
 
 // .onclick
 
+let page = "home";
+
 let liArray = document.querySelectorAll("li") // [li, li, li]
 liArray.forEach(li => {
     li.onclick = function() {
@@ -35,6 +37,71 @@ liArray.forEach(li => {
 
         // bittasiga qo'shish
         li.classList.add("active");
+
+        console.log(li.id);
+
+        page = li.id;
+
+        switch(page) {
+            case "home": {
+                document.querySelector("#root").innerHTML = 
+                    `<h1>Home bu</h1>`;
+                break;
+            }
+
+            case "short": {
+                document.querySelector("#root").innerHTML = 
+                    `<h1>Short bu</h1>`;
+                break;
+            }
+
+            case "subscription": {
+                document.querySelector("#root").innerHTML = 
+                    `<h1>Subscribtion bu</h1>`;
+                break;
+            }
+
+            case "channel": {
+                document.querySelector("#root").innerHTML = 
+                    `<h1>Channel bu</h1>`;
+                break;
+            }
+
+            case "history": {
+                document.querySelector("#root").innerHTML = 
+                    `<h1>History bu</h1>`;
+                break;
+            }
+
+            case "playlists": {
+                document.querySelector("#root").innerHTML = 
+                    `<h1>Playlists bu</h1>`;
+                break;
+            }
+
+            case "videos": {
+                document.querySelector("#root").innerHTML = 
+                    `<h1>Playlists bu</h1>`;
+                break;
+            }
+
+            case "later": {
+                document.querySelector("#root").innerHTML = 
+                    `<h1>Later bu</h1>`;
+                break;
+            }
+
+            case "like": {
+                document.querySelector("#root").innerHTML = 
+                    `<h1>Like bu</h1>`;
+                break;
+            }
+
+            default: {
+                document.querySelector("#root").innerHTML = 
+                    `<h1>404 not found</h1>`;
+            }
+        }
     }
 });
 
